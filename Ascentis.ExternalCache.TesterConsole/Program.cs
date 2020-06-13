48d747498f451223e75423f3573dba4d63b13ea8
Sebastian Battig <sbattig@ascentis.com>
2020-06-12 22:36:02 -0300

    Added console performance test serializing json as byte[]

--- Ascentis.ExternalCache.TesterConsole/Program.cs
+++ Ascentis.ExternalCache.TesterConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Ascentis.ExternalCache.TesterConsole;
+using Utf8Json;
 
 namespace Ascentis.Infrastructure
 {
@@ -12,6 +13,7 @@ namespace Ascentis.Infrastructure
             externalCache.Select("PerformanceTestCache");
             externalCache.Clear();
 
+            Console.WriteLine(@"--- Executing serializing a string ---");
             var initialTickCount = Environment.TickCount;
             Console.WriteLine($@"Start:{initialTickCount}");
             for (var i = 0; i < count; i++)
@@ -33,6 +35,19 @@ namespace Ascentis.Infrastructure
                 externalCache.Get($"Item{i}");
             Console.WriteLine($@"Finish:{Environment.TickCount}");
             Console.WriteLine($@"Speed (insert/retrieves per second): {(count / (((float) (Environment.TickCount - initialTickCount)) / 1000))}");
+            externalCacheManager.ClearAllCaches();
+
+            Console.WriteLine(@"--- Executing serializing JSON as byte[] ---");
+            initialTickCount = Environment.TickCount;
+            Console.WriteLine($@"Start:{initialTickCount}");
+            dynamic obj2 = new Dynamo { ["Prop1"] = TextResource.SampleXML };
+            var jsonObj = JsonSerializer.Serialize(obj2); 
+            for (var i = 0; i < count; i++)
+                externalCache.Add($"Item{i}", jsonObj);
+            for (var i = 0; i < count; i++)
+                externalCache.Get($"Item{i}");
+            Console.WriteLine($@"Finish:{Environment.TickCount}");
+            Console.WriteLine($@"Speed (insert/retrieves per second): {(count / (((float)(Environment.TickCount - initialTickCount)) / 1000))}");
             externalCacheManager.ClearAllCaches();
 
             Console.WriteLine(@"Press any key to finish");
