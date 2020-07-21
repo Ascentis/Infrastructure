b9e83d43e1149efb94f2f0e94ddd2327181ecdca
Jose Sebastian Battig <sebastian.battig@ascentis.com>
2020-07-21 13:57:19 -0300

    Added unit test for Cleanable pattern

---
+++ Ascentis.Infrastructure.Test/UnitTestCleanable.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// ReSharper disable once CheckNamespace
+namespace Ascentis.Infrastructure.Test
+{
+    [TestClass]
+    public class UnitTestCleanable
+    {
+        [TestMethod]
+        public void TestCleanable()
+        {
+            var cleaned = false;
+            {
+                using var cleanable = new Cleanable<int>(1, value => cleaned = true);
+                Assert.AreEqual(1, cleanable.Value);
+            }
+            Assert.IsTrue(cleaned);
+        }
+    }
+}
