7bc38d21ee3daad473399d7b8ed94ef944b7ac48
Jose Sebastian Battig <sebastian.battig@ascentis.com>
2020-08-03 14:17:18 -0300

    removed unnecesary public from interface declaration

--- Ascentis.Infrastructure/SQLUtils/ISqlStreamerFormatter.cs
+++ Ascentis.Infrastructure/SQLUtils/ISqlStreamerFormatter.cs
@@ -7,9 +7,9 @@ namespace Ascentis.Infrastructure
 {
     public interface ISqlStreamerFormatter
     {
-        public void Prepare(SqlDataReader reader, Stream stream);
-        public void Process(object[] row, Stream stream);
-        public void UnPrepare(Stream stream);
-        public void AbortedWithException(Exception e);
+        void Prepare(SqlDataReader reader, Stream stream);
+        void Process(object[] row, Stream stream);
+        void UnPrepare(Stream stream);
+        void AbortedWithException(Exception e);
     }
 }
