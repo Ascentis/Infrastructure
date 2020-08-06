d6fc8f299ea14842dd7e755126cf0b655274ca02
Jose Sebastian Battig <sebastian.battig@ascentis.com>
2020-08-06 19:37:37 -0300

    Added support for handling enumerable target adapters

--- Ascentis.Infrastructure/DataPipeline/SourceAdapter/Text/DelimitedDataPipeline.cs
+++ Ascentis.Infrastructure/DataPipeline/SourceAdapter/Text/DelimitedDataPipeline.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace Ascentis.Infrastructure.DataPipeline.SourceAdapter.Text
 {
@@ -18,6 +19,44 @@ namespace Ascentis.Infrastructure.DataPipeline.SourceAdapter.Text
                 Delimiter = delimiter
             };
             base.Pump(sourceAdapter, dataPipelineTargetAdapter);
+        }
+
+        public void Pump(
+            Stream source,
+            DataPipelineColumnMetadata[] sourceMetadatas,
+            IDataPipelineTargetAdapter<PoolEntry<object[]>> dataPipelineTargetAdapter,
+            string delimiter = DataPipelineDelimitedSourceAdapter.DefaultDelimiter,
+            int rowsPoolCapacity = DataPipelineTextSourceAdapter.DefaultRowsPoolCapacity)
+        {
+            using var reader = new StreamReader(source);
+            Pump(reader, sourceMetadatas, dataPipelineTargetAdapter, delimiter, rowsPoolCapacity);
+        }
+
+        public void Pump(
+            TextReader source,
+            DataPipelineColumnMetadata[] sourceMetadatas,
+            IEnumerable<IDataPipelineTargetAdapter<PoolEntry<object[]>>> dataPipelineTargetAdapters,
+            string delimiter = DataPipelineDelimitedSourceAdapter.DefaultDelimiter,
+            int rowsPoolCapacity = DataPipelineTextSourceAdapter.DefaultRowsPoolCapacity)
+        {
+            var sourceAdapter = new DataPipelineDelimitedSourceAdapter(source)
+            {
+                ColumnMetadatas = sourceMetadatas,
+                RowsPoolCapacity = rowsPoolCapacity,
+                Delimiter = delimiter
+            };
+            base.Pump(sourceAdapter, dataPipelineTargetAdapters);
+        }
+
+        public void Pump(
+            Stream source,
+            DataPipelineColumnMetadata[] sourceMetadatas,
+            IEnumerable<IDataPipelineTargetAdapter<PoolEntry<object[]>>> dataPipelineTargetAdapters,
+            string delimiter = DataPipelineDelimitedSourceAdapter.DefaultDelimiter,
+            int rowsPoolCapacity = DataPipelineTextSourceAdapter.DefaultRowsPoolCapacity)
+        {
+            using var reader = new StreamReader(source);
+            Pump(reader, sourceMetadatas, dataPipelineTargetAdapters, delimiter, rowsPoolCapacity);
         }
     }
 }
