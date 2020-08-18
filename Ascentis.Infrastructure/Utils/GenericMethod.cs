0cb6837ee5d647c62ba407ae51a44826536734a7
Jose Sebastian Battig <sebastian.battig@ascentis.com>
2020-08-18 00:56:01 -0300

    Added small utility class to create a delegate from delegate type, target class and method name

---
+++ Ascentis.Infrastructure/Utils/GenericMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+// ReSharper disable once CheckNamespace
+namespace Ascentis.Infrastructure
+{ 
+    public static class GenericMethod
+    {
+        public static TDelegate BuildMethodDelegate<TDelegate, TClass>(string methodName) where TDelegate : Delegate
+        {
+            var type = typeof(TClass);
+            var methodInfo = type.GetMethod(methodName, BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+            ArgsChecker.CheckForNull<NullReferenceException>(methodInfo, $"Method {methodName} not found in {typeof(TClass).Name} class");
+            // ReSharper disable once PossibleNullReferenceException
+            return (TDelegate)methodInfo.CreateDelegate(typeof(TDelegate));
+        }
+    }
+}
