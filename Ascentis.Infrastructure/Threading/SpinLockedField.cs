c2d167e4a2eaa87d8f980dafe33c3bff3becf0ef
Jose Sebastian Battig <sebastian.battig@ascentis.com>
2020-09-08 16:31:13 -0300

    Added new class

---
+++ Ascentis.Infrastructure/Threading/SpinLockedField.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+
+// ReSharper disable once CheckNamespace
+namespace Ascentis.Infrastructure
+{
+    public class SpinLockedField<T>
+    {
+        private SpinLock _lock;
+        private T _field;
+
+        public SpinLockedField()
+        {
+            _lock = new SpinLock();
+        }
+
+        public T Get()
+        {
+            var lockTaken = false;
+            try
+            {
+                _lock.Enter(ref lockTaken);
+                return _field;
+            }
+            finally
+            {
+                if (lockTaken)
+                    _lock.Exit();
+            }
+        }
+
+        public void Set(T value)
+        {
+            var lockTaken = false;
+            try
+            {
+                _lock.Enter(ref lockTaken);
+                _field = value;
+            }
+            finally
+            {
+                if (lockTaken)
+                    _lock.Exit();
+            }
+        }
+    }
+}
