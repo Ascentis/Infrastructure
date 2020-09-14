5dc44fbf0e4fb9538b78692c463c2d07c640257a
Jose Sebastian Battig <sebastian.battig@ascentis.com>
2020-09-14 10:24:59 -0300

    removed some more warnings

--- Ascentis.Infrastructure/DataPipeline/TargetAdapter/Sql/Utils/MetaToParamSettings.cs
+++ Ascentis.Infrastructure/DataPipeline/TargetAdapter/Sql/Utils/MetaToParamSettings.cs
@@ -8,11 +8,11 @@ namespace Ascentis.Infrastructure.DataPipeline.TargetAdapter.Sql.Utils
 {
     public class MetaToParamSettings
     {
-        public IEnumerable<string> Columns { get; set; }
-        public IDictionary<string, int> ColumnToIndexMap { get; set; }
-        public ColumnMetadataList Metadatas { get; set; }
-        public IEnumerable<string> AnsiStringParameters { get; set; }
-        public DbParameterCollection Target { get; set; }
+        public IEnumerable<string>? Columns { get; set; }
+        public IDictionary<string, int>? ColumnToIndexMap { get; set; }
+        public ColumnMetadataList? Metadatas { get; set; }
+        public IEnumerable<string>? AnsiStringParameters { get; set; }
+        public DbParameterCollection? Target { get; set; }
         public bool UseShortParam { get; set; }
         public bool UseDefaultSuffix { get; set; } = true;
         public string? ParamSuffix { get; set; }
